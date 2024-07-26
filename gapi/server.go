2a232eded688a9664588be52c41f6718cd898188
almoghindi <almoghinde@gmail.com>
2024-07-26 23:28:50 +0300

    deploy

---
+++ gapi/server.go
@@ -0,0 +1,37 @@
+package gapi
+
+import (
+	"fmt"
+
+	db "github.com/almoghindi/simplebank/db/sqlc"
+	"github.com/almoghindi/simplebank/pb"
+	"github.com/almoghindi/simplebank/token"
+	"github.com/almoghindi/simplebank/util"
+	"github.com/almoghindi/simplebank/worker"
+)
+
+// Server serves gRPC requests for our banking service.
+type Server struct {
+	pb.UnimplementedSimpleBankServer
+	config          util.Config
+	store           db.Store
+	tokenMaker      token.Maker
+	taskDistributor worker.TaskDistributor
+}
+
+// NewServer creates a new gRPC server.
+func NewServer(config util.Config, store db.Store, taskDistributor worker.TaskDistributor) (*Server, error) {
+	tokenMaker, err := token.NewPasetoMaker(config.TokenSymmetricKey)
+	if err != nil {
+		return nil, fmt.Errorf("cannot create token maker: %w", err)
+	}
+
+	server := &Server{
+		config:          config,
+		store:           store,
+		tokenMaker:      tokenMaker,
+		taskDistributor: taskDistributor,
+	}
+
+	return server, nil
+}
